99d2c7cdc09ea470dfbd61d419491f47c5e7faa3
王先生 <15718810177@163.com>
2024-05-02 23:47:43 +0800

    wx

--- SilkRenderer.WinUI/OpenGL/RenderContext.cs
+++ SilkRenderer.WinUI/OpenGL/RenderContext.cs
@@ -67,7 +67,6 @@ public unsafe class RenderContext
 
             IWindow window = Window.Create(options);
 
-            window.CreateWindow(options);
             window.Initialize();
 
             GL = window.CreateOpenGL();
